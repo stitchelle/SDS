6a279886f5728abfe4ca0ecb9f0f47c43afbccbd
Michelle Johnson <bob1234girl@gmail.com>
2020-03-16 10:21:10 -0500

    subject form addes a new subject

---
+++ sdsapp/views/subjects/form.py
@@ -0,0 +1,32 @@
+import sqlite3
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from sdsapp.models import Subject
+from sdsapp.models import model_factory
+from ..connection import Connection
+
+
+def get_subjects():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(Subject)
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        select
+            s.id,
+            s.name
+        from sdsapp_subject s
+        """)
+
+        return db_cursor.fetchall()
+
+@login_required
+def subject_form(request):
+    if request.method == 'GET':
+        subjects = get_subjects()
+        template = 'subjects/form.html'
+        context = {
+            'all_subjects': subjects
+        }
+
+        return render(request, template, context)
