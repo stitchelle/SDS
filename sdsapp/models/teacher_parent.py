e4ff08b4dd15d2f43f1fe4827f19bca87c8fdbd3
Michelle Johnson <bob1234girl@gmail.com>
2020-03-23 10:17:28 -0500

    register but fixed

--- sdsapp/models/teacher_parent.py
+++ sdsapp/models/teacher_parent.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 class Teacher_Parent(models.Model):
 
@@ -9,33 +7,6 @@ class Teacher_Parent(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    is_teacher = models.BooleanField(null=True)
+    is_teacher = models.BooleanField(default=True)
 
-
-# These receiver hooks allow you to continue to
-# work with the `User` class in your Python code.
-
-# Every time a `User` is created, a matching `Teacher_Parent`
-# object will be created and attached as a one-to-one
-# property
-@receiver(post_save, sender=User)
-def create_teacher_parent(sender, instance, created, **kwargs):
-    if created:
-        Teacher_Parent.objects.create(user=instance)
-
-# Every time a `User` is saved, its matching `Teacher_Parent`
-# object will be saved.
-@receiver(post_save, sender=User)
-def save_teacher_parent(sender, instance, **kwargs):
-    instance.teacher_parent.save()
-
-    class Meta:
-        verbose_name = ("teacher_parent")
-        verbose_name_plural = ("teacher_parents")
-
-    def __str__(self):
-        return self.name
-        
-    def get_absolute_url(self):
-        return reverse("teacher_parent_detail", kwargs={"pk": self.pk})
     
